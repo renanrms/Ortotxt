71ef4a288c6120a69772fb82599311de39758f5e
renanrms <renanpassos@poli.ufrj.br>
2018-06-04 06:07:32 -0300

    Acertos

--- dict/simplificar.pl
+++ dict/simplificar.pl
@@ -6,38 +6,29 @@ use strict;
 
 open (my $out, ">", "dict_pt-br.txt") or die "Can't open dict_pt-br.txt: $!";
 
-my @AtoZ = split(//, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); #Obtém um vetor com cada letra do alfabeto.
+my @AtoZ = split (//, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"); # Obtém um vetor com cada letra do alfabeto.
 
-foreach my $letra (@AtoZ){ 
+foreach my $letra (@AtoZ){ # Para cada letra...
 
 	open (my $in, "<", "$letra.txt") or die "Can't open $letra.txt: $!";
 	
 	while (<$in>){
-		if ($_ =~ /\*( \*)?[a-zA-Z]*\*.*/){
+		if ($_ =~ /\*( \*)?[a-zA-Z]*\*.*/){ # testa se a linha comtém uma palavra.
 			my @linha = split (//, $_);
 			my $count = 0;
 			
-			while (@linha[$count] !~ /[a-zA-Z]/) { $count++; }
+			#Obtém o início da palavra.
+			while (@linha[$count] !~ /[a-zA-Z]/) { $count++; } 
 			my $inicio = $count;
 			
+			#Obtém o fim da palavra e calcula o tamanho.
 			while (@linha[$count] =~ /[a-zA-Z]/) { $count++; }
 			my $tamanho = $count - $inicio;			
 			
-			my $palavra = substr $_, $inicio, $tamanho;
-			print $out lc "$palavra\n";
+			# Obtém a palavra em caixa baixa e grava no arquivo.
+			my $palavra = lc (substr ($_, $inicio, $tamanho));
+			print $out "$palavra\n";
 		}
-
-# método antigo:		
-#		my @linha;
-#		if ($_ =~ /\* \*[a-zA-Z]*\*.*/){
-#			@linha = split (/\* \*/, $_);
-#			@linha = split (/\*/, @linha[1]);
-#			print $out "@linha[0]\n";
-#		}
-#		elsif ($_ =~ /\*[a-zA-Z]*\*.*/){
-#			@linha = split (/\*/, $_);
-#			print $out "@linha[1]\n";
-#		}
 	}
 
 	close ($in) or die "cant't close $letra.txt: $!";
